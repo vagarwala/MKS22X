6a5e6727eeba9d46213b1cf06f69ac227f452874
Vandana Agarwala <vandana.agarwala@gmail.com>
2016-03-29 14:13:33 -0400

    idk what changes i made, forgot to push earlier

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -1,5 +1,4 @@
 public class ParenDemo{
-    /*
     public static boolean isMatching(String str){
         if (str.isEmpty())
             return true;
@@ -20,37 +19,6 @@ public class ParenDemo{
             }
         }
         return stack.isEmpty();
-    }
-    */
-    public static boolean isMatching(String input) {
-
-        char [] openChars =  {'[','{','('};
-        char [] closeChars = {']','}',')'};
-
-        MyStack<Character> stack = new MyStack<Character>();
-
-        for (int i = 0; i < input.length(); i++) {
-
-            String x = "" +input.charAt(i);
-
-            if (String.valueOf(openChars).indexOf(x) != -1)
-                stack.push(input.charAt(i));
-            else{
-                Character lastOpener = stack.peek();
-                int idx1 = String.valueOf(openChars).indexOf(lastOpener.toString());
-                int idx2 = String.valueOf(closeChars).indexOf(x);
-
-                if (idx1 != idx2)
-                    return false;
-                else
-                    stack.pop();
-            }
-        }
-
-        if (stack.size() == 0)
-            return true;
-        else
-            return false;
     }
     public static void main(String[] args){
         String input = "";
