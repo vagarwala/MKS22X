7714cda75b47660b0f66c6d92771e095502a26c3
Vandana Agarwala <vandana.agarwala@gmail.com>
2016-03-29 12:26:07 -0400

    gitignore?

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -1,24 +1,56 @@
 public class ParenDemo{
+    /*
     public static boolean isMatching(String str){
         if (str.isEmpty())
             return true;
         MyStack<Character> stack = new MyStack<Character>();
         for (int i = 0; i < str.length(); i++){
             char current = str.charAt(i);
-            if (current == '{' || current == '(' || current == '['){
+            if (current == '{' || current == '(' || current == '[' || current == '<'){
                 stack.push(current);
             }
-            if (current == '}' || current == ')' || current == ']'){
+            if (current == '}' || current == ')' || current == ']' || current == '>'){
                 if (stack.isEmpty())
                     return false;
                 char last = stack.peek();
-                if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[')
+                if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[' || current == '>' && last == '<')
                     stack.pop();
                 else 
                     return false;
             }
         }
         return stack.isEmpty();
+    }
+    */
+    public static boolean isMatching(String input) {
+
+        char [] openChars =  {'[','{','('};
+        char [] closeChars = {']','}',')'};
+
+        MyStack<Character> stack = new MyStack<Character>();
+
+        for (int i = 0; i < input.length(); i++) {
+
+            String x = "" +input.charAt(i);
+
+            if (String.valueOf(openChars).indexOf(x) != -1)
+                stack.push(input.charAt(i));
+            else{
+                Character lastOpener = stack.peek();
+                int idx1 = String.valueOf(openChars).indexOf(lastOpener.toString());
+                int idx2 = String.valueOf(closeChars).indexOf(x);
+
+                if (idx1 != idx2)
+                    return false;
+                else
+                    stack.pop();
+            }
+        }
+
+        if (stack.size() == 0)
+            return true;
+        else
+            return false;
     }
     public static void main(String[] args){
         String input = "";
